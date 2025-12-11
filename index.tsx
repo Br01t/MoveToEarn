f5f76c533ae4c0fb3378efd5bb2a4fe198b09927
Br01t <giacomo.piccinini1@gmail.com>
2025-12-11 23:54:49 +0100

    fix

--- index.tsx
+++ index.tsx
@@ -1,14 +1,15 @@
 // index.tsx
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root element not found");
 
-ReactDOM.render(
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
