a31a90f2d7e235c10a8a4d8276360cd88e207c3e
Br01t <giacomo.piccinini1@gmail.com>
2025-12-17 20:13:09 +0100

    build fix

--- sw.ts
+++ sw.ts
@@ -1,11 +1,18 @@
 
+// @ts-nocheck
+/* eslint-disable no-restricted-globals */
+
+// Nome della cache per questa versione
 const CACHE_NAME = 'zonerun-cache-v1';
+
+// Risorse base da salvare per il funzionamento offline
 const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json'
 ];
 
+// Installazione: scarica gli asset nella cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -14,6 +21,7 @@ self.addEventListener('install', (event) => {
   );
 });
 
+// Fetch: serve i file dalla cache se presenti, altrimenti usa la rete
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
